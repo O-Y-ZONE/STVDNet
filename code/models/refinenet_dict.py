6cbc51a520a2c6b3683a376be6d5a4c43f8a5add
O-Y-ZONE <74223895+O-Y-ZONE@users.noreply.github.com>
2023-06-01 11:27:47 +0800

    Add files via upload

---
+++ code/models/refinenet_dict.py
@@ -0,0 +1,10 @@
+# from models.R_CLSTM_modules import (R_CLSTM_1, R_CLSTM_2, R_CLSTM_3,
+#                                             R_CLSTM_4, R_CLSTM_5, R_CLSTM_6,
+#                                             R_CLSTM_7, R_CLSTM_8, R_CLSTM_9)
+
+from models.STSEI_modules import STSEI_5, STSEI_6
+
+refinenet_dict = {
+    'STSEI_5': STSEI_5,
+    'STSEI_6': STSEI_6,
+}
